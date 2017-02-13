b058a6e5c86d7219381a47a7d4b4f262fb504335
hugo bosse <monsieurbosse@gmail.com>
2017-02-13 14:19:25 +0100

    the complete stupid coach exerise (no css)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get '/answer' => 'coaching#answer'
+
+  get '/' => 'coaching#ask'
+
+  get 'pages/contact'
+
+  get 'pages/about'
+
+  get 'contact/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
